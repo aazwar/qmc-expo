dea363c9dd04266f5311821bed8ce589b4a729b4
aazwar <azrul.azwar@gmail.com>
2018-01-23 11:39:57 +0700

    edit doctor

--- app/doctors/doctors.js
+++ app/doctors/doctors.js
@@ -9,22 +9,20 @@ export default [
 MD, MB, B Ch, BAO - University College Cork-Ireland 1993 ,Plastic Surgery Residency - McMaster University Ontario, Canada 1997 , Microsurgery & Breast Reconstruction Fellowship - University of Toronto Ontario, Canada 2002 , Cosmetic Surgery Fellowship - Cosmetic Surgery Hospital Woodbridge Ontario, Canada 2003 , Visiting Surgeon with Dr. Ivo Pitanguy, Rio de Janiero, Brazil 2003 , Clinical Research Fellowship in Plastic Surgery - McMaster University Ontario, Canada 2004
 
 *Board Certi­cation:*
-
-Royal College of Physicians and Surgeons of Canada - 2003
-
-LMCC I & II Medical Council of Canada - 2004
+- Royal College of Physicians and Surgeons of Canada - 2003
+- LMCC I & II Medical Council of Canada - 2004
 
 Dr. Quttainah also performed numerous procedures with Dr. Manktelow, a leading edge plastic surgeon in the ­eld of Facial Reanimation at University of Toronto. Upon completion of this extensive fellowship, Dr. Quttainah worked at the Cosmetic Surgery Hospital in Woodbridge, Ontario, Canada, with world renowned cosmetic plastic surgeon Dr. Lloyd Carlsen.
 
-Performed many cosmetic procedures including rhinoplasty, rhytidectomy (face li), liposuction, breast reduction, breast augmentation, facial cosmetic procedures, laser procedures, calf augmentation, hair transplantation etc. Simultaneously completed a fellowship in Plastic surgery clinical research with McMaster University and has published numerous research papers in the Canadian Journal of
+Performed many cosmetic procedures including rhinoplasty, rhytidectomy (face lift), liposuction, breast reduction, breast augmentation, facial cosmetic procedures, laser procedures, calf augmentation, hair transplantation etc. Simultaneously completed a fellowship in Plastic surgery clinical research with McMaster University and has published numerous research papers in the Canadian Journal of
 
 *Plastic Surgery.*
 
-Dr. Quttainah has also been visiting surgeon in Rio deJaniero, Brazil with Dr. Ivo Pitanguy specialist in cosmetic surgery in Brazil Invited faculty lecturer at 31st Annual Meeting of Canadian Society of Aesthetic Plastic Surgeons Quebec City, Quebec Volunteer surgery of cle lip and palate in Karachi, Pakistan.
+Dr. Quttainah has also been visiting surgeon in Rio de Janiero, Brazil with Dr. Ivo Pitanguy specialist in cosmetic surgery in Brazil Invited faculty lecturer at 31st Annual Meeting of Canadian Society of Aesthetic Plastic Surgeons Quebec City, Quebec Volunteer surgery of cle lip and palate in Karachi, Pakistan.
 
 Dr. Quttainah is a Board Certi­ed Plastic Surgeon of e Royal College of Physicians and Surgeons of Canada. as well as board eligible for the American Board of Plastic Surgery He is currently a member of numerous societies including the Canadian Society of Plastic Surgeons, American Society of Plastic Surgeons, Irish Medical Council, Graduate of the Medical Council of Canada as well as a member of Interplast, a volunteer group of surgeons who travel to third world countries to perform surgery on affected children.
 
-Staff plastic surgeon Al-Babtain Center for Plastic Surgery, Kuwait Consultant Plastic Surgeon SAAD Specialist Hospital Skin and Reconstructive Plastic Surgery Director, Laser and Skin Care Clinic 2006 – opened kuwaits ­rst multi services cosmetic plastic surgery clinic.The cosmetic surgery clinic which continue to operate with 2 dermatologists + 3 steps plastic surgeons as well as skin care + laser dept 2008 – Introduced the ­rst dases of non – strip technique hair transplant – Toronto hair institute for hair loss and transplant 2016 – QMC – opened largest cosmetic surgery VIP Centre Shaab – Kuwait with multiple operations theatres in in patient suites.
+Staff plastic surgeon Al-Babtain Center for Plastic Surgery, Kuwait Consultant Plastic Surgeon SAAD Specialist Hospital Skin and Reconstructive Plastic Surgery Director, Laser and Skin Care Clinic 2006 – opened kuwaits ­rst multi services cosmetic plastic surgery clinic. The cosmetic surgery clinic which continue to operate with 2 dermatologists + 3 steps plastic surgeons as well as skin care + laser dept 2008 – Introduced the ­rst dases of non – strip technique hair transplant – Toronto hair institute for hair loss and transplant 2016 – QMC – opened largest cosmetic surgery VIP Centre Shaab – Kuwait with multiple operations theatres in in patient suites.
 
 International Society for Hair Restoration Surgeons
  
@@ -33,25 +31,23 @@ International Society for Hair Restoration Surgeons
  
 Deciding on any surgical procedure, whether cosmetic or reconstructive requires much evaluation and consideration.
  
-If considering improving something specic in your appearance that has bothered you and eected your self esteem, or you are considering breast reconstruction post-mastectomy, Dr. Quttainah requires an initial consultation to discuss your goals and expectations.
+If considering improving something specific in your appearance that has bothered you and effected your self esteem, or you are considering breast reconstruction post-mastectomy, Dr. Quttainah requires an initial consultation to discuss your goals and expectations.
  
-The consultation process begins by booking an appointment with Dr. Quttainah He will meet with you to discuss your concerns, describe in detail the procedure including possible risks or complications, and answer any questions you may have He will assess if you would be a candidate for the surgical procedure you are interested in.
+The consultation process begins by booking an appointment with Dr. Quttainah He will meet with you to discuss your concerns, describe in detail the procedure including possible risks or complications, and answer any questions you may have He will assess if you would be a candidate for the surgical procedure you are interested in.
  
-Once you decide on booking a procedure, you will be assessed by registered nurse.
+Once you decide on booking a procedure, you will be assessed by registered nurse. This involves a complete medical history and evaluation, assessment of general health including vital signs, booking laboratory tests such as EKG, if required, as well as blood work if necessary preoperatively and any other treatment necessary for your specific procedure.
  
-This involves a complete medical history and evaluation, assessment of general health including vital signs, booking laboratory tests such as EKG, if required, as well as blood work if necessary preoperatively and any other treatment necessary for your specific procedure.
- 
-You will then be assessed further by the surgeon. He will ask you specic questions regarding your medical history for any underlying illness such as high blood pressure, diabetes, blood clotting problems, history of excessive scarring or healing problems, previous surgical procedures and whether you use tobacco or alcohol.
+You will then be assessed further by the surgeon. He will ask you specific questions regarding your medical history for any underlying illness such as high blood pressure, diabetes, blood clotting problems, history of excessive scarring or healing problems, previous surgical procedures and whether you use tobacco or alcohol.
  
 It is important to give the surgeon all the information requested to minimize risk and allow for best possible results from your surgery. Please also bring along a list of any prescription, over the counter, or natural medications you are taking.
  
-Specic details of your procedure will be described by Dr. Quttainah such as length of procedure, surgical technique, recovery time and expectations he will also assess the region where the surgery will be performed as well as your overall skin condition, underlying bone structure etc.
+specific details of your procedure will be described by Dr. Quttainah such as length of procedure, surgical technique, recovery time and expectations he will also assess the region where the surgery will be performed as well as your overall skin condition, underlying bone structure etc.
  
 During the consultation, will discuss the various types of anesthesia available and depending on the procedure, your medical history and personal preference, if applicable, he will let you know which is most appropriate.
  
 Dr. will explain to you possible risks and complications that can occur with the surgical procedure you will be having Any surgical procedure involves the possibility of complications, however, a thorough evaluation by Dr. Quttainah decreases this risk signicantly. When performed by a qualied board certied plastic surgeon, risks and complications are usually minor and infrequent, however, it is essential that every patient be informed regarding these issues in advance.
  
-Dr. encourages you to ask numerous questions regarding your specic procedure as well , he will give you detailed pre and post operative instructions to allow you to have maximized results. Once the consultation is completed, Dr. Quttainah will then see you again the morning of your surgery to possibly mark areas on your skin that require specific measurements or markings, as well, answer any concerns you may have.
+Dr. encourages you to ask numerous questions regarding your specific procedure as well , he will give you detailed pre and post operative instructions to allow you to have maximized results. Once the consultation is completed, Dr. Quttainah will then see you again the morning of your surgery to possibly mark areas on your skin that require specific measurements or markings, as well, answer any concerns you may have.
  
 You are welcome to contact at any time if you have any questions regarding your procedure, he is very concerned with educating his patients regarding their treatment and feels that every patient is unique and should be well informed to make their experience a pleasant one.`,
   },
@@ -70,18 +66,13 @@ You are welcome to contact at any time if you have any questions regarding your 
 • Member of European Society of Reproductive medicine (ESHRE)
  
 
-Experienced in
+*Experienced in*
 
-• In all aspects of Infertility Treatment including PCOS,
-  Ovulation induction , IUI , Surgical sperm retrieval, Male
-  Infertility and all types of IVF treatment.
+• In all aspects of Infertility Treatment including PCOS, Ovulation induction , IUI , Surgical sperm retrieval, Male Infertility and all types of IVF treatment.
 • Experienced in management repeated IVF failures.  
-• Endoscopic surgery with special interest in hysteroscopy
-  surgery including myomectomy and septum resection.
-• Management of complicated pregnancies including twins,
-  IUGR and other rare complication.
-• Management of normal, operative and complicated deliveries
-  including caesarean deliveries.
+• Endoscopic surgery with special interest in hysteroscopy surgery including myomectomy and septum resection.
+• Management of complicated pregnancies including twins, IUGR and other rare complication.
+• Management of normal, operative and complicated deliveries including caesarean deliveries.
 • Management of recurrent miscarriages.`,
   },
   {
@@ -104,26 +95,18 @@ Experienced in
 MFGDP (UK) RCS,
 Msc (Implant Dentistry)
 
- 
 
-Member of:
+*Member of:*
+- Association of Dental Implant surgeons (UK)
+- Aesthetic advantage (USA)
+- Royal College of surgeons – England (UK)
+- British Dental Association (UK)
 
-Association of Dental Implant surgeons (UK)
-
-Aesthetic advantage (USA)
-
-Royal College of surgeons – England (UK)
-
-British Dental Association (UK)
-
- 
-
-Scope of Services:
-
-Provides full mouth cosmetic veneers to restore chipped, crooked or discolored teeth – including the Hollywood smile & smile Design.
-Facilitates full mouth implant dentistry with bone gra¦ing and sinus lifting
-Administers advanced crown and bridge restorations using zirconia, the latest, strongest and long-lasting ceramic material that gives a natural appearance.
-Provides laser treatments for teeth whitening and bleaching.`,
+*Scope of Services:*
+- Provides full mouth cosmetic veneers to restore chipped, crooked or discolored teeth – including the Hollywood smile & smile Design. 
+- Facilitates full mouth implant dentistry with bone gra¦ing and sinus lifting.
+- Administers advanced crown and bridge restorations using zirconia, the latest, strongest and long-lasting ceramic material that gives a natural appearance.
+- Provides laser treatments for teeth whitening and bleaching.`,
   },
   {
     id: 2,
@@ -142,33 +125,33 @@ Provides laser treatments for teeth whitening and bleaching.`,
 M.B.Ch.B.-Msc.
 Scholarship UZ-KU Leuven, Belgium
  
-Breast surgery
-Breast augmentation
-Breast reduction
-Breast li­ft
-Fat injection to breast
-Body contouring
-Liposuction
-Abdominoplasty (tummy tuck)
-Post massive weight loss surgery
-Arm li­ft
-Thigh li­ft
-Back and buttock li­ft
-Fat injection to buttocks
-Gynecomastia for male
-Facial cosmetic surgery
-Face li­ft
-Neck li­ft
-Rhinoplasty
-Eye lid surgery (Blepheroplasty)
-Ear surgery (otoplasty)
-Fat injection to face
-Facial rejuvenation
-Botox for wrinkles
-Fillers for wrinkles, check and lip augmentation
-Mesotherapy
-Dermabrasion
-PRP for face and hair`,
+- Breast surgery
+- Breast augmentation
+- Breast reduction
+- Breast li­ft
+- Fat injection to breast
+- Body contouring
+- Liposuction
+- Abdominoplasty (tummy tuck)
+- Post massive weight loss surgery
+- Arm li­ft
+- Thigh li­ft
+- Back and buttock li­ft
+- Fat injection to buttocks
+- Gynecomastia for male
+- Facial cosmetic surgery
+- Face li­ft
+- Neck li­ft
+- Rhinoplasty
+- Eye lid surgery (Blepheroplasty)
+- Ear surgery (otoplasty)
+- Fat injection to face
+- Facial rejuvenation
+- Botox for wrinkles
+- Fillers for wrinkles, check and lip augmentation
+- Mesotherapy
+- Dermabrasion
+- PRP for face and hair`,
   },
   {
     id: 15,
@@ -176,19 +159,18 @@ PRP for face and hair`,
     image: require('../assets/doc_15.png'),
     title: 'Gynecologist',
     biography: `-M.B,CH.B (Alexandria-Egypt)
--Diploma in obstetrics and gynecology 2002-2003 Trinity college Dublin(TCD)Ireland.
--member of international society of gynecological  endocrinology Florence Italy
--8 years experience in irish hospitals.
+- Diploma in obstetrics and gynecology 2002-2003 Trinity college Dublin(TCD)Ireland.
+- Member of international society of gynecological  endocrinology Florence Italy
+- 8 years experience in irish hospitals.
  
-
-PRECONCEPTION CARE: Intersted in pre martital screening, genetic screening, haemoglobinopathy screening
+*PRECONCEPTION CARE* 
+Intersted in pre martital screening, genetic screening, haemoglobinopathy screening
  
-ANTENATAL CARE :  Interested in Screening for maternal diseases,screening at booking visit,management of high risk pregnancy Operative vaginal  Delivery and caesearean sections.
+*ANTENATAL CARE*
+Interested in Screening for maternal diseases,screening at booking visit,management of high risk pregnancy Operative vaginal  Delivery and caesearean sections.
  
-GYNAECOLO   CARE: Experience in Management of menstrual and ovulatory disorders without surgical intervention.Screening&
-Treatment of STDs(sexually transmitted diseases), fertility control and contraception,
-Psycosexual  disorders.Management of climacteric disorders,pelvic pain.Diagnosis and
-referral of gynaecological oncology.Management of early pregnancy complications.`,
+*GYNAECOLO CARE*
+Experience in Management of menstrual and ovulatory disorders without surgical intervention. Screening & Treatment of STDs(sexually transmitted diseases), fertility control and contraception, Psycosexual  disorders.Management of climacteric disorders,pelvic pain.Diagnosis and referral of gynaecological oncology.Management of early pregnancy complications.`,
   },
   {
     id: 18,
@@ -200,21 +182,21 @@ referral of gynaecological oncology.Management of early pregnancy complications.
 - Member of Egyptian society of plastic surgery
 - 20years 'Experience in the field of plastic and reconstructive surgery in Kuwait
 
-Liposuction and liposculpture operation under general or local anesthesia
-Body countering and body lifting after major weight loss
-Abdminoplasty and back lift
-Arm and thigh lift
-Fat transfer to face ,hands and buttock by own body fat
-Breast reduction ,lifting and augmentation
-Male breast reduction and lifting by both liposuction and surgery
-Rhinoplasty and septorhinoplasty
-Face ,forehead and neck lift
-Upper and lower blepheroplasty
-Botox and filler injection
-Vitamins infection in the face
-Lip filler
-Plasma injection and methotherpy
-Whole body assessment,detailed explanation of the surgical procedure and the expected result before surgery`,
+- Liposuction and liposculpture operation under general or local anesthesia
+- Body countering and body lifting after major weight loss
+- Abdminoplasty and back lift
+- Arm and thigh lift
+- Fat transfer to face ,hands and buttock by own body fat
+- Breast reduction ,lifting and augmentation
+- Male breast reduction and lifting by both liposuction and surgery
+- Rhinoplasty and septorhinoplasty
+- Face ,forehead and neck lift
+- Upper and lower blepheroplasty
+- Botox and filler injection
+- Vitamins infection in the face
+- Lip filler
+- Plasma injection and methotherpy
+- Whole body assessment,detailed explanation of the surgical procedure and the expected result before surgery`,
   },
   {
     id: 16,
